6ced0572adb00ef015fbfa1b36a805eae165c3ad
darwin osingo <darwin.osingo@student.moringaschool.com>
2025-03-20 20:25:22 +0300

    initialcommit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber'; // Test 1 + 2
+
+let mostProfitableNeighborhood = 'Chelsea'; // Test 3 + 4
+
+let companyCeo = 'Susan Smith'; // Test 5 + 6
+
